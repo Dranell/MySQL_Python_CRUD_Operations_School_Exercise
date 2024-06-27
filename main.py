7a95b404ca468b954ae728c3547c624613d2ac6b
Dranell <dranellwilson!@gmail.com>
2024-06-27 08:49:20 -0500

    Created DB from main.py

--- main.py
+++ main.py
@@ -1,20 +1,42 @@
 import  mysql.connector
 from mysql.connector import Error
 
+import create_database_query as cdq
 
-def createServerConnection(host_name,user_name,user_password):
+
+def createServerConnection(host_name,user_name,user_password,db_name):
     connection = None
     try:
         connection = mysql.connector.connect(
             host = host_name,
             user = user_name,
-            passwd = user_password
+            passwd = user_password,
+            database = db_name
+
         )
         print("MySQL Database Connection Successful!!")
 
     except Error as err:
         print(f"Error:{err}")
-
     return  connection
+#create a function that will create a database
+def create_database(connection,query):
+    cursor = connection.cursor()
+    try:
+        cursor.execute(query)
+        print("Database created successfully!!")
+    except Error as err:
+        print(f"Error: {err}")
+
+#create work horse function to execute queries
+def execute_query(connection,query):
+    cursor = connection.cursor()
+    try:
+        cursor.execute(query)
+        connection.commit()
+        print("Query successful!!")
+    except Error as err:
+        print(f"Error: '{err}'")
+
 #call function to establish connection from main.py to mySQL
-createServerConnection("localhost","root","student")
+connection = createServerConnection("localhost","root","student","school")
